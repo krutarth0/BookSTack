ec63d668b08e2647417bb52a97d1e1fd1b1f6f9d
krutarth0 <krutarthzero@hotmail.com>
2019-03-02 13:13:19 +0530

    testing updates

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ class App extends Component {
     return (
         <ApolloProvider client={client}>
             <div id="main">
-                <h1 className="logo-font">Book STack v1.0</h1>
+                <h1 className="logo-font">Book STack </h1>
                 <BookList />
 
             </div>
